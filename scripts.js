cedee7288af9740e60408bbf31fe62ab2863ad0d
Ronald Gutierrez <ronaldgutierrezj@gmail.com>
2023-07-13 16:02:18 -0500

    take off Button code

--- scripts.js
+++ scripts.js
@@ -1,2 +1,16 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+function takeOff (){
+   let response = confirm ("Confirm that shuttle is ready for takeoff")
+
+   if(response){
+    let p = document.getElementById("flightStatus");
+    p.innerHTML = "Shuttle in flight";
+    let div = document.getElementById("shuttleBackground");
+    div.style.backgroundColor = "blue";
+    let height = document.getElementById("spaceShuttleHeight")
+    height.innerHTML = Number(height.innerHTML) + 10
+   } else{
+    console.log("ok, let's try next time")
+   }
+}
